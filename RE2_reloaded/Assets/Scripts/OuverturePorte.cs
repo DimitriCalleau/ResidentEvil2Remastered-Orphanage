0a1c975f76d1dbbc54db92ca570a945fb6dc9c9d
DimitriCalleau <dimitri.calleau@outlook.fr>
2019-05-18 16:05:08 +0200

    dfsfd

--- RE2_reloaded/Assets/Scripts/OuverturePorte.cs
+++ RE2_reloaded/Assets/Scripts/OuverturePorte.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class OuverturePorte : MonoBehaviour
 {
+    private bool isOpenedRight;
+    private bool isOpenedLeft;
+    private bool detectRight;
+    private bool detectLeft;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,48 @@ public class OuverturePorte : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Ray rightRay = new Ray(transform.position, Vector3.right);
+        Ray leftRay = new Ray(transform.position, Vector3.left);
+        if(Physics.Raycast(rightRay, out hit))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                detectRight = true;
+            }
+            else
+            {
+                detectRight = false;
+            }
+        }
+        if (Physics.Raycast(leftRay, out hit))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                detectLeft = true;
+            }
+            else
+            {
+                detectLeft = false;
+            }
+        }
+
     }
-}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            if (detectRight == true)
+            {
+
+            }
+            
+            if (detectLeft == true)
+            {
+
+            }
+        }
+    }
+} 
+
